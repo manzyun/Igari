230d8878337a7b95c9f067afc782e19752574841
manzyun <manzyun@gmail.com>
2026-01-31 16:20:05 +0900

    [Add] シーン入れの為のメソッドのみ定義
    TODO兼ねて`

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,3 +4,17 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+# 以下、各シーンをプリロードする予定。
+# 参考資料： Godot Thirdperson`Shooting Demo
+func title():
+	pass
+
+func running_game():
+	pass
+
+func gameover():
+	pass
+
+func ending():
+	pass
